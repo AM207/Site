f5a274531c9dd8e2bfc1c3e99a0124b0c053b96b
Rahul Dave <rahuldave@gmail.com>
2014-01-29 16:45:29 -0500

    no need for ssh user in ssh_upload

--- am207/pelicanconf.py
+++ am207/pelicanconf.py
@@ -16,7 +16,7 @@ DEFAULT_LANG = u'en'
 DEFAULT_DATE = 'fs'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM =  "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
